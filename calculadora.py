4687985b4b637d05dbe382bcdc6818ff2c87def6
Matheus <mcmatheuscaio@gmail.com>
2025-06-09 14:25:43 -0300

    melhoria divisao

--- calculadora.py
+++ calculadora.py
@@ -8,3 +8,11 @@ def multiplicar(a, b):
     return a*b
 
 print(multiplicar(2, 3))
+
+#branch divisão
+
+def dividir(a, b):
+    if b != 0:
+        return a / b
+    return "erro:  divisão por zero"
+
